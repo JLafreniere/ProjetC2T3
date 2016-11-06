22b91036eecd3b8a53e77d5f880e0cb9ce502b42
JLafreniere <jonathan.lafreniere@hotmail.com>
2016-11-06 13:58:39 -0800

    Ajout du projet ASP.Net

---
+++ App MVC/App/App/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public object SendAlert()
+        {
+
+
+            //insérer un objet alerte dans la base de données
+
+
+            return View();
+        }
+
+    }
+}
